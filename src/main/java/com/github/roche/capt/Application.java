c14ae1d578be10af26a15a1837af5c2dc8d59f09
bikram <bikramjit.singh@roche.com>
2025-03-26 23:50:06 -0700

    clean up

--- src/main/java/com/github/roche/capt/Application.java
+++
@@ -1,7 +0,0 @@
-package com.github.roche.capt;
-
-public class Application {
-    public static void main(String[] args) {
-        System.out.println("Hello, this is the capt-copybara project!");
-    }
-}
